aba9ab0efdf3cada41080ad2b9a9c364bd7db8c0
hiendv <hien.dv.neo@gmail.com>
2019-09-13 02:57:35 +0700

    docs(vue-tabs-example): improve the example

--- packages/vue-tabs-example/src/App.vue
+++ packages/vue-tabs-example/src/App.vue
@@ -2,66 +2,81 @@
   <div id="app">
     <div class="container">
       <header>
-        <h1>@hiendv/vue-tabs</h1>
+        <h1><a href="https://github.com/hiendv/tabs/tree/master/packages/vue-tabs">@hiendv/vue-tabs</a></h1>
+        <iframe
+          src="https://ghbtns.com/github-btn.html?user=hiendv&repo=tabs&type=star&count=true&size=large" frameborder="0"
+          scrolling="0" width="160px"
+          height="30px"
+        />
       </header>
       <section>
         <h3>Default themes</h3>
-        <div :style="{ padding: '1rem', backgroundColor: isThemeDefault ? '#fff' : '#313131' }">
+        <div :style="{ padding: '1rem', backgroundColor: isDefault ? '#fff' : '#313131' }">
           <tabs :theme="theme">
             <tab title="Foo" :icon="globe">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-              tempor incididunt ut labore et dolore magna aliqua.
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore.
             </tab>
 
             <tab
-              title="Bar" :icon="zap"
-              hash="bar"
+              title="Bar" hash="bar"
+              :icon="zap"
             >
-              Refresh and I'm still here because the URL fragment is <strong>#bar</strong> now.
+              Refresh and I'm still here because the URL fragment is
+              <strong>#bar</strong> now.
             </tab>
 
             <tab
-              title="Qux" :icon="beaker"
-              hash="qux"
+              title="Qux" hash="qux"
+              :icon="beaker"
             >
               Hi, my name is <strong>#qux</strong>
             </tab>
 
             <tab
-              :title="isThemeDefault ? 'Dark' : 'Default'" :icon="flame"
+              :title="themeText" :icon="flame"
               end ghost
               @click.prevent="switchTheme"
             />
           </tabs>
+          <hr>
+          <snippet :code="sourceDefault" style="max-height: 400px" />
         </div>
       </section>
       <section>
         <h3>Custom theme & navigation</h3>
         <div style="background-color: #fff; padding: 1rem">
-          <tabs :show.sync="show" :theme="{panel: 'custom-tab-panel'}">
+          <tabs :show.sync="show" :theme="themeCustom">
             <template v-slot:nav="{ items }">
               <a
                 v-for="(item, index) in items" :key="index"
-                href="#" :class="{ 'custom-tab-item': true, 'active': show === index }"
+                href="#" :class="{ 'custom-item': true, 'active': show === index }"
                 @click.prevent="show = index"
               >{{ item.title }}</a>
             </template>
-            <tab title="First" :icon="globe">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-              tempor incididunt ut labore et dolore magna aliqua.
+            <tab title="First">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna.
             </tab>
 
-            <tab title="Second" :icon="zap">
-              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-              aliquip ex ea commodo consequat.
+            <tab title="Second">
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco
+              laboris nisi ut aliquip ex ea commodo consequat.
             </tab>
 
-            <tab title="Third" :icon="beaker">
-              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-              dolore eu fugiat nulla pariatur.
+            <tab title="Third">
+              Duis aute irure dolor in reprehenderit in voluptate velit
+              esse cillum dolore eu fugiat nulla pariatur.
             </tab>
           </tabs>
+
+          <hr>
+          <snippet :code="sourceCustom" style="max-height: 400px" />
         </div>
+      </section>
+      <section>
+        <h3>API Documentation</h3>
+        <p>See more here: <a href="https://github.com/hiendv/tabs/tree/master/packages/vue-tabs">vue-tabs</a></p>
       </section>
     </div>
   </div>
@@ -70,19 +85,159 @@
 <script>
 import { globe, zap, beaker, flame } from 'octicons-vue'
 import { Tabs, Tab, themeDefault, themeDark } from '@hiendv/vue-tabs'
+import Snippet from './Snippet.vue'
 export default {
   name: 'App',
   components: {
     Tabs,
-    Tab
+    Tab,
+    Snippet
   },
   data () {
     return {
       show: 1,
-      theme: themeDefault
+      theme: themeDefault,
+      themeText: 'Default',
+      themeCustom: {
+        tabs: 'custom-tabs',
+        items: 'custom-items',
+        item: 'custom-item',
+        panel: 'custom-panel'
+      }
     }
   },
   computed: {
+    isDefault () {
+      return this.themeText === 'Default'
+    },
+    /* eslint-disable no-useless-escape */
+    sourceDefault () {
+      return `<template>
+  <tabs>
+    <tab title="Foo">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+      sed do eiusmod tempor incididunt ut labore et dolore.
+    </tab>
+
+    <tab title="Bar" hash="bar">
+      Refresh and I'm still here because the URL fragment is
+      <strong>#bar</strong> now.
+    </tab>
+
+    <tab title="Qux" hash="qux">
+      Hi, my name is <strong>#qux</strong>
+    </tab>
+
+    <tab
+      :title="themeText" :icon="flame"
+      end ghost
+      @click.prevent="switchTheme"
+    />
+  </tabs>
+</template>
+<script>
+import { globe, zap, beaker, flame } from 'octicons-vue'
+import { Tabs, Tab } from '@hiendv/vue-tabs'
+export default {
+  components: {
+    Tabs,
+    Tab,
+  },
+  data () {
+    return {
+      globe,
+      zap,
+      beaker,
+      flame,
+      theme: themeDefault,
+      themeText: 'Default'
+    }
+  },
+  switchTheme () {
+    if (this.themeText === 'Dark') {
+      this.themeText = 'Dark'
+      this.theme = themeDark
+      return
+    }
+
+    this.themeText = 'Default'
+    this.theme = themeDefault
+  }
+}
+<\/script>`
+    },
+    sourceCustom () {
+      return `<template>
+  <tabs :show.sync="show" :theme="themeCustom">
+    <template v-slot:nav="{ items }">
+      <a
+        v-for="(item, index) in items" :key="index"
+        href="#" @click.prevent="show = index"
+        :class="{
+          'custom-item': true,
+          'active': show === index
+        }"
+      >{{ item.title }}</a>
+    </template>
+    <tab title="First">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+      sed do eiusmod tempor incididunt ut labore et dolore magna.
+    </tab>
+
+    <tab title="Second">
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat.
+    </tab>
+
+    <tab title="Third">
+      Duis aute irure dolor in reprehenderit in voluptate velit
+      esse cillum dolore eu fugiat nulla pariatur.
+    </tab>
+  </tabs>
+</template>
+<script>
+import { Tabs, Tab } from '@hiendv/vue-tabs'
+export default {
+  components: {
+    Tabs,
+    Tab,
+  },
+  data () {
+    return {
+      show: 1,
+      themeCustom: {
+        tabs: 'custom-tabs',
+        items: 'custom-items',
+        item: 'custom-item',
+        panel: 'custom-panel'
+      }
+    }
+  }
+}
+<\/script>
+<style lang="scss">
+.custom-item {
+  display: inline-block;
+  background-color: #7599ff;
+  color: #fff;
+  padding: .75rem 1rem;
+  border-radius: 4px;
+  margin-right: 2px;
+  text-decoration: none;
+  &.active {
+    background-color: #5e65a8;
+  }
+}
+
+.custom-panel {
+  margin-top: 2px;
+  padding: 1rem;
+  color: #253140;
+  background-color: #f9fafe;
+}
+</style>`
+    },
+    /* eslint-enable no-useless-escape */
     globe () {
       return globe
     },
@@ -94,18 +249,17 @@ export default {
     },
     flame () {
       return flame
-    },
-    isThemeDefault () {
-      return this.theme === themeDefault
     }
   },
   methods: {
     switchTheme () {
-      if (this.isThemeDefault) {
+      if (this.isDefault) {
+        this.themeText = 'Dark'
         this.theme = themeDark
         return
       }
 
+      this.themeText = 'Default'
       this.theme = themeDefault
     }
   }
@@ -127,12 +281,22 @@ section {
   margin-bottom: 2rem;
 }
 
+h1 {
+  a {
+    text-decoration: none;
+    color: #444;
+    &:hover {
+      color: #000;
+    }
+  }
+}
+
 .container {
   margin: 0 auto;
   max-width: 600px;
 }
 
-.custom-tab-item {
+.custom-item {
   display: inline-block;
   background-color: #7599ff;
   color: #fff;
@@ -145,7 +309,7 @@ section {
   }
 }
 
-.custom-tab-panel {
+.custom-panel {
   margin-top: 2px;
   padding: 1rem;
   color: #253140;
---
+++ packages/vue-tabs-example/src/Snippet.vue
@@ -0,0 +1,188 @@
+<script>
+import Prism from 'prismjs/components/prism-core.js'
+
+require('prismjs/components/prism-markup.js')
+require('prismjs/components/prism-clike.js')
+require('prismjs/components/prism-javascript.js')
+require('prismjs/components/prism-bash.js')
+require('prismjs/components/prism-css.js')
+
+export default {
+  functional: true,
+  props: {
+    code: {
+      type: String,
+      default: ''
+    },
+    language: {
+      type: String,
+      default: 'markup'
+    },
+    inline: {
+      type: Boolean,
+      default: false
+    }
+  },
+  render (h, ctx) {
+    const code = ctx.props.code || (ctx.children && ctx.children.length > 0 ? ctx.children[0].text : '')
+    const language = ctx.props.language
+    const prismLanguage = Prism.languages[language]
+    const className = `language-${language}`
+
+    if (process.env.NODE_ENV === 'development' && !prismLanguage) {
+      throw new Error(`Prism component for language "${language}" was not found`)
+    }
+
+    const codeBlock = h('code', {
+      class: [ ctx.data.class, className ],
+      domProps: {
+        innerHTML: Prism.highlight(code || '', prismLanguage)
+      }
+    })
+
+    if (ctx.props.inline) {
+      return codeBlock
+    }
+
+    return h('pre', { class: [ ctx.data.class, className ], style: ctx.data.staticStyle }, [ codeBlock ])
+  }
+}
+</script>
+<style lang="scss">
+/**
+ * prism.js default theme for JavaScript, CSS and HTML
+ * Based on dabblet (http://dabblet.com)
+ * @author Lea Verou
+ */
+
+/* stylelint-disable selector-max-type, selector-no-qualifying-type */
+code[class*="language-"],
+pre[class*="language-"] {
+  font-family: Consolas, Monaco, "Andale Mono", "Ubuntu Mono", monospace;
+  hyphens: none;
+  line-height: 1.5;
+  color: #374961;
+  text-align: left;
+  text-shadow: 0 1px #eff2f6;
+  word-break: normal;
+  word-spacing: normal;
+  word-wrap: normal;
+  tab-size: 2;
+  white-space: pre;
+  background: none;
+}
+
+@media print {
+  code[class*="language-"],
+  pre[class*="language-"] {
+    text-shadow: none;
+  }
+}
+
+:not(pre) > code[class*="language-"],
+pre[class*="language-"] {
+  background: #f5f5f5;
+}
+
+pre[class*="language-"] {
+  padding: 1em;
+  margin: 0.5em 0;
+  overflow: auto;
+}
+
+pre[class*="language-"]::-moz-selection,
+pre[class*="language-"] ::-moz-selection,
+code[class*="language-"]::-moz-selection,
+code[class*="language-"] ::-moz-selection {
+  text-shadow: none;
+  background: #b3d4fc;
+}
+
+pre[class*="language-"]::selection,
+pre[class*="language-"] ::selection,
+code[class*="language-"]::selection,
+code[class*="language-"] ::selection {
+  text-shadow: none;
+  background: #b3d4fc;
+}
+
+:not(pre) > code[class*="language-"] {
+  padding: 0.1em;
+  white-space: normal;
+  border-radius: 0.3em;
+}
+
+.token.comment,
+.token.prolog,
+.token.doctype,
+.token.cdata {
+  color: #808080;
+}
+
+.token.punctuation {
+  color: #999;
+}
+
+.namespace {
+  opacity: 0.7;
+}
+
+.token.property,
+.token.tag,
+.token.boolean,
+.token.number,
+.token.constant,
+.token.symbol,
+.token.deleted {
+  color: #905;
+}
+
+.token.selector,
+.token.attr-name,
+.token.string,
+.token.char,
+.token.builtin,
+.token.inserted {
+  color: #690;
+}
+
+.token.operator,
+.token.entity,
+.token.url,
+.language-css .token.string,
+.style .token.string {
+  color: #9a6e3a;
+  background: hsla(0, 0%, 100%, 0.5);
+}
+
+.token.atrule,
+.token.attr-value,
+.token.keyword {
+  color: #07a;
+}
+
+.token.function,
+.token.class-name {
+  color: #dd4a68;
+}
+
+.token.regex,
+.token.important,
+.token.variable {
+  color: #e90;
+}
+
+.token.important,
+.token.bold {
+  font-weight: bold;
+}
+
+.token.italic {
+  font-style: italic;
+}
+
+.token.entity {
+  cursor: help;
+}
+/* stylelint-enable */
+</style>
