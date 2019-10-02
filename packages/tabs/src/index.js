bff5b06e853f90cb7a9db9fd02a4f12b2ec9d72b
unknown <villa7.kluge5@gmail.com>
2019-10-01 23:36:22 -0400

    feat: material theme

--- packages/tabs/src/index.js
+++ packages/tabs/src/index.js
@@ -1,4 +1,6 @@
 export { default as themeDefault } from '../themes/default.module.scss'
 export { default as themeDark } from '../themes/dark.module.scss'
+export { default as themeMaterial } from '../themes/material.module.scss'
+export { default as themeMaterialDark } from '../themes/material-dark.module.scss'
 export { setHash } from './location'
 export { deepmerge } from './merge'
